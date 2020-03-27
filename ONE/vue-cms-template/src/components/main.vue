f5b4fb08b64b913cce62fcada1695b4ab748e7e8
Hongnnn <408359913@qq.com>
2020-03-27 15:45:13 +0800

    chu

---
+++ ONE/vue-cms-template/src/components/main.vue
@@ -0,0 +1,65 @@
+<template>
+  <div >
+    <!-- <header> -->
+     
+      <!-- <routr-view>
+        table -->
+        <router-view></router-view>
+        <nav class="mui-bar mui-bar-tab">
+          <router-link class="mui-tab-item" to="/"  name="home" exact="" >
+            <span class="mui-icon mui-icon-home"></span>
+              <span class="mui-tab-label">首页
+            </span>
+          </router-link >
+               <router-link  class="mui-tab-item" to="/find"  name="find" >
+            <span class="mui-icon mui-icon-eye"></span>
+               <span class="mui-tab-label">发现
+            </span>
+          </router-link >
+             <router-link   class="mui-tab-item" to="/fa" name="fa" >
+            <span class="mui-icon mui-icon-plus"></span>
+               <span class="mui-tab-label">发布
+            </span>
+          </router-link >
+         
+           <router-link   class="mui-tab-item" to="/order" name="order" >
+            <span class="mui-icon mui-icon-bars"></span>
+               <span class="mui-tab-label">订单
+            </span>
+          </router-link >
+           <router-link  class="mui-tab-item" to="/my" name="my"  >
+            <span class="mui-icon mui-icon-person"></span>
+               <span class="mui-tab-label">我的
+            </span>
+          </router-link >
+        </nav>  
+  
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
+
+
+<script>
+export default {
+  data(){
+    return{
+      name:this.$route.name,
+    }
+  }
+}
+</script>
+
+<style>
+.router-link-active{
+    color: aqua;
+}
+
+</style>
+ 
---
+++ ONE/vue-cms-template/src/main.js
@@ -0,0 +1,62 @@
+// 入口文件
+import Vue from 'vue'
+import app from './App.vue'
+import Vuex from 'vuex'
+// import { Header, Swipe, SwipeItem,Navbar, TabItem} from 'mint-ui'
+import  VueRouter  from 'vue-router'
+import VueSource from 'vue-resource'
+import router  from './router.js'
+import 'mint-ui/lib/style.css'
+import MintUI from 'mint-ui'
+import axios from 'axios'
+import qs  from  'qs'
+// import store from './store/datatoken'
+
+import axiosInit from "./store/axios-init";
+
+
+
+Vue.use(MintUI)
+Vue.use(VueRouter)
+Vue.use(VueSource)
+Vue.use(Vuex)
+
+Vue.prototype.$axios = axios;
+
+
+// Vue.component(Header.name,Header)
+// Vue.component(Swipe.name,Swipe)
+// Vue.component(SwipeItem.name,SwipeItem)
+// Vue.component(Navbar.name, Navbar);
+// Vue.component(TabItem.name, TabItem);
+
+
+
+
+import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+import './style/rem'
+
+
+
+
+var vm = new Vue({
+
+    el:'#app'   , 
+    render:c =>c(app),
+    router,
+    // store
+
+})
+// axios.interceptors.request.use(
+//     config => {
+//       if (localStorage.getItem('Authorization')) {
+
+//         config.headers.Authorization = localStorage.getItem('Authorization');
+//       }
+   
+//       return config;
+//     },
+//     error => {
+//       return Promise.reject(error);
+//     });
