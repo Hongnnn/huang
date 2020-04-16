021b4115426838503d25da715351c3ed84ec2b73
Hongnnn <408359913@qq.com>
2020-04-16 17:57:34 +0800

    订单详情搞定

--- ONE/vue-cms-template/src/components/order/order-detail.vue
+++ ONE/vue-cms-template/src/components/order/order-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="detail">
-    <div >
+    <div>
       <mt-header fixed title="订单详情">
         <router-link to="/" slot="left">
           <mt-button icon="back"></mt-button>
@@ -10,52 +10,109 @@
     <div class="order-lists">
       <p>订单列表</p>
       <ul>
-        <li>
+        <li v-for="(item,index) in params.orderlist" :key="index">
           <div class="orderd-content">
             <div class="orderd-left">
               <img src="../../style/img/aa.jpg" alt />
-              <h5>书本</h5>
+              <h5>{{item.name}}</h5>
             </div>
-            <h5 class="orderd-count">数量：1</h5>
-            <h5>￥1</h5>
+            <h5 class="orderd-count">*{{item.count}}</h5>
+            <h5>￥{{item.price}}</h5>
           </div>
-          <div class="orderd-jifen"><h5> 总积分：111</h5>
-          </div>
-          
         </li>
+        <div class="orderd-jifen">
+          <h5>总积分：{{totalprice}}</h5>
+        </div>
       </ul>
     </div>
     <div class="order-infor">
-        <p>订单信息</p>
-        <h4>下单人:111</h4>
-        <h4>手机号码:111</h4>
-        <h4>下单时间:111</h4>
-
+      <p>订单信息</p>
+      <h4>下单人:{{params.ordername}}</h4>
+      <h4>手机号码:{{params.orderphone}}</h4>
+      <h4>下单时间:{{ordertime}}</h4>
     </div>
-      <div class="order-huishou">
-        <p>回收信息</p>
-        <h4>回收柜：11</h4>
-        <h4>密码：44</h4>
-        <h4>处理人:111</h4>
-        <h4>手机号码:111</h4>
-        
+    <div class="order-huishou">
+      <p>回收信息</p>
+      <h4>回收柜：{{params.ordernumber}}</h4>
+      <h4>密码：{{params.orderpsw}}</h4>
+      <h4>处理人:{{params.receptname}}</h4>
+      <h4>手机号码:{{params.receptphone}}</h4>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      params: [],
+      orderid: ""
+    };
+  },
+  created() {
+    this.getDetail();
+  },
+  computed: {
+    totalprice() {
+      let total = 0;
+      let bb = [];
+      bb = this.params.orderlist;
+      if (!bb) {
+        return;
+      }
+      bb.forEach(food => {
+        total += food.price * food.count;
+      });
+      return total;
+    },
+    ordertime() {
+      let oldtime = this.params.ordertime;
+      let date = new Date(oldtime);
+      let y = date.getFullYear();
+      let MM = date.getMonth() + 1;
+      MM = MM < 10 ? "0" + MM : MM;
+      let d = date.getDate();
+      d = d < 10 ? "0" + d : d;
+      let h = date.getHours();
+      h = h < 10 ? "0" + h : h;
+      let m = date.getMinutes();
+      m = m < 10 ? "0" + m : m;
+      let s = date.getSeconds();
+      s = s < 10 ? "0" + s : s;
+      return y + "-" + MM + "-" + d + " " + h + ":" + m + ":" + s;
+    }
+  },
+  methods: {
+    getDetail() {
+      let orderid = this.$route.query.userid;
+      console.log(orderid);
+      let url = `http://106.15.136.244:8080/order/getOrderDetail?orderId=${orderid}`;
+      console.log(url);
+      this.$axios
+        .get(url)
+        .then(res => {
+          this.params = res.data.data;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
+</script>
 <style lang="scss" scoped>
-.detail{
-    position: relative;
+.detail {
+  position: relative;
 }
 .order-lists {
-    margin-top: .8rem;
+  margin-top: 0.8rem;
   width: 100%;
   background: white;
   padding: 0.2rem;
-  li{
-      display: block;
+  li {
+    display: block;
   }
-  p{
-      font-size: .35rem;
+  p {
+    font-size: 0.35rem;
     color: black;
   }
 }
@@ -79,29 +136,28 @@
     width: 23%;
   }
 }
-.orderd-jifen{
-       padding-left: 78%;
-    h5{
-            color: black;
-    }
-}
-.order-infor,.order-huishou{
-   margin-top: .2rem;
-    background: white;
-    padding: 0.2rem;
-    p{
-            font-size: .35rem;
+.orderd-jifen {
+  padding-left: 78%;
+  h5 {
     color: black;
-    }
-    h4{
-            font-size: .3rem;
-    padding-bottom: .1rem;
-    font-weight: 400;
-    }
-    
+  }
 }
-.order-huishou{
-    top:48%
-
+.order-infor,
+.order-huishou {
+  margin-top: 0.2rem;
+  background: white;
+  padding: 0.2rem;
+  p {
+    font-size: 0.35rem;
+    color: black;
+  }
+  h4 {
+    font-size: 0.3rem;
+    padding-bottom: 0.1rem;
+    font-weight: 400;
+  }
+}
+.order-huishou {
+  top: 48%;
 }
 </style>>
--- ONE/vue-cms-template/src/components/tabbar/order.vue
+++ ONE/vue-cms-template/src/components/tabbar/order.vue
@@ -13,18 +13,51 @@
     <!-- tab-container -->
     <mt-tab-container v-model="selected">
       <mt-tab-container-item id="1">
-        <div class="order">
+        <div class="order" >
           <div class="order-list">
-            <ul>
-              <li v-for="item in list">
+              <ul>
+              <li v-for="(item,index) in newArr " :key="index" @click="detail(item.orderId)">
                 <div class="order-header">
                   <p>
-                    订单编号:{{item.id}}
+                    订单编号:{{item.orderId}}
                     <span>已接单</span>
                   </p>
                 </div>
                 <ul>
-                  <li v-for="good in item.olist">
+                  <li v-for="good in item.orderList" > 
+                    <div class="order-content">
+                      <div class="order-left">
+                        <img :src="good.icon" alt />
+                        <h5>{{good.name}}</h5>
+                      </div>
+                      <h5 class="order-count">数量：{{good.count}}</h5>
+                      <h5>￥{{good.price}}</h5>
+                    </div>
+                  </li>
+                </ul>
+
+                <div class="order-footer">
+                  <button>查看详情</button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="no-order" v-show="notOrder">暂无订单~</div>
+      </mt-tab-container-item>
+      <mt-tab-container-item id="2">
+        <div class="order">
+          <div class="order-list">
+            <ul>
+              <li v-for="(item,index) in newArr" :key="index" @click="detail(item.orderId)">
+                <div class="order-header">
+                  <p>
+                    订单编号:{{item.orderId}}
+                    <span>未接单</span>
+                  </p>
+                </div>
+                <ul>
+                  <li v-for="good in item.orderList"> 
                     <div class="order-content">
                       <div class="order-left">
                         <img :src="good.icon" alt />
@@ -44,26 +77,32 @@
             </ul>
           </div>
         </div>
+           <div class="no-order" v-show="notOrder">暂无订单~</div>
       </mt-tab-container-item>
-      <mt-tab-container-item id="2">
+      <mt-tab-container-item id="3">
         <div class="order">
           <div class="order-list">
             <ul>
-              <li v-for="item in orderList">
+              <li v-for="item in newArr">
                 <div class="order-header">
                   <p>
-                    订单编号:{{item.orderid}}
-                    <span>未接单</span>
+                    订单编号:{{item.orderId}}
+                    <span>已完成</span>
                   </p>
                 </div>
-                <div class="order-content">
-                  <div class="order-left">
-                    <img :src="item.icon" alt />
-                    <h5>{{item.name}}</h5>
-                  </div>
-                  <h5 class="order-count">数量：{{item.count}}</h5>
-                  <h5>￥{{item.price}}</h5>
-                </div>
+                <ul>
+                  <li v-for="good in item.orderList"> 
+                    <div class="order-content">
+                      <div class="order-left">
+                        <img :src="good.icon" alt />
+                        <h5>{{good.name}}</h5>
+                      </div>
+                      <h5 class="order-count">数量：{{good.count}}</h5>
+                      <h5>￥{{good.price}}</h5>
+                    </div>
+                  </li>
+                </ul>
+
                 <div class="order-footer">
                   <button>查看详情</button>
                 </div>
@@ -71,60 +110,40 @@
             </ul>
           </div>
         </div>
-      </mt-tab-container-item>
-      <mt-tab-container-item id="3">
-        <div class="order">
-          <div class="order-list">
-            <ul>
-              <li>
-                <div class="order-header">
-                  <p>
-                    订单编号:
-                    <span>已回收</span>
-                  </p>
-                </div>
-                <div class="order-content">
-                  <div class="order-left">
-                    <img src="../../style/img/aa.jpg" alt />
-                    <h5>书本</h5>
-                  </div>
-                  <h5 class="order-count">数量：1</h5>
-                  <h5>￥1</h5>
-                </div>
-                <div class="order-footer">
-                  <button>查看柜号</button>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
+           <div class="no-order" v-show="notOrder">暂无订单~</div>
       </mt-tab-container-item>
       <mt-tab-container-item id="4">
         <div class="order">
           <div class="order-list">
             <ul>
-              <li>
+              <li v-for="item in newArr">
                 <div class="order-header">
                   <p>
-                    订单编号:
+                    订单编号:{{item.orderId}}
                     <span>已取消</span>
                   </p>
                 </div>
-                <div class="order-content">
-                  <div class="order-left">
-                    <img src="../../style/img/aa.jpg" alt />
-                    <h5>书本</h5>
-                  </div>
-                  <h5 class="order-count">数量：1</h5>
-                  <h5>￥1</h5>
-                </div>
+                <ul>
+                  <li v-for="good in item.orderList"> 
+                    <div class="order-content">
+                      <div class="order-left">
+                        <img :src="good.icon" alt />
+                        <h5>{{good.name}}</h5>
+                      </div>
+                      <h5 class="order-count">数量：{{good.count}}</h5>
+                      <h5>￥{{good.price}}</h5>
+                    </div>
+                  </li>
+                </ul>
+
                 <div class="order-footer">
-                  <button>查看柜号</button>
+                  <button>查看详情</button>
                 </div>
               </li>
             </ul>
           </div>
         </div>
+           <div class="no-order" v-show="notOrder">暂无订单~</div>
       </mt-tab-container-item>
     </mt-tab-container>
   </div>
@@ -136,8 +155,10 @@ export default {
   name: "nav-title",
   data() {
     return {
+     
       selected: "1",
-      orderList: [],
+      orderist: [],
+      notOrder:false,
       list: [
         {
           orderid: "111",
@@ -145,65 +166,62 @@ export default {
             {
               name: "书本",
               price: "5",
-              icon: "http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
+              icon:
+                "http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
               count: "1"
             },
             {
               name: "报纸",
               price: "5",
-              icon:"http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
+              icon:
+                "http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
               count: "1"
             }
-          ],
-         
+          ]
         },
-          {
+        {
           id: "1221",
           olist: [
             {
               name: "书本",
               price: "5",
-              icon: "http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
+              icon:
+                "http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
               count: "1"
             },
             {
               name: "报纸",
               price: "5",
-              icon:"http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
+              icon:
+                "http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
               count: "1"
             }
-          ],
-         
+          ]
         },
-         {
+        {
           orderid: "1221",
           orderlist: [
             {
               name: "书本",
               price: "5",
-              icon: "http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
+              icon:
+                "http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
               count: "1"
             },
             {
               name: "报纸",
               price: "5",
-              icon:"http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
+              icon:
+                "http://static.galileo.xiaojukeji.com/static/tms/default_header.png",
               count: "1"
             }
-          ],
-         
+          ]
         }
-        
       ],
-      aa:[
-      {
-        ordeid:'',
-        li:[
-
-        ]
-      }
-
-      ]
+      aa: [],
+      newArr: [],
+      
+      id: ""
     };
   },
   watch: {
@@ -218,13 +236,47 @@ export default {
         this.$axios
           .get(url)
           .then(response => {
-            this.orderList = response.data.data;
-            
+           this.newArr=[]
+            //  const data =JSON.parse(response.data.data)
+            this.newobj = response.data.data;
+            if(this.newobj.length==0){
+             this.notOrder=true
+            }
+            else{
+            let id = 0;
+            this.newobj.forEach((v, i) => {
+              id = v[i].orderid;
+              // console.log('id值',this.newobj[i]);
+              const orderList = [];
+              v.forEach(v => {
+                orderList.push({
+                  name: v.name,
+                  price: v.price,
+                  icon: v.icon,
+                  count: v.count
+                });
+              });
+              this.newArr.push({
+                orderId: id,
+                orderList: orderList
+              });
+            })};
+
+            console.log(this.newArr)
           })
           .catch(function(error) {
             console.log(error);
           });
       }
+    }
+  },
+  methods:{
+    detail(data){
+       this.$router.push({
+                path: '/orderdetail',
+                query:{userid:data}
+            })
+
     }
   }
 };
